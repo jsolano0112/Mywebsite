704e2319407fa5df118f37c56e2e32b9e16bc257
Juana Solano <juana.solano@correo.tdea.edu.co>
2023-04-30 17:15:29 -0500

    adding imgs

--- js/codigo.js
+++ js/codigo.js
@@ -7,17 +7,19 @@ let opcionDeProyecto
 
 //Clase para proyecto
 class Proyecto {
-    constructor(nombre, link, imagen, texto) {
+    constructor(nombre, link, imagen, ancho, alto, texto) {
         this.nombre = nombre
         this.link = link
         this.imagen = imagen
+        this.ancho = ancho
+        this.alto = alto
         this.texto = texto
     }
 }
 
-let pizarra = new Proyecto('PIZARRA', 'https://jsolano0112.github.io/Pizarra/', '', '')
-let appControlGastos = new Proyecto('APP CONTROL GASTOS', 'https://github.com/jsolano0112/ControlGastos.git', '', '')
-let gusanoPy = new Proyecto('GUSANITO-PYTHON', 'https://github.com/jsolano0112/Gusanito.git', '', '')
+let pizarra = new Proyecto('PIZARRA', 'https://jsolano0112.github.io/Pizarra/', 'img/pizarra.png',300,200, '')
+let appControlGastos = new Proyecto('APP CONTROL GASTOS', 'https://github.com/jsolano0112/ControlGastos.git', 'img/appControlGastos.png',200,200, '')
+let gusanoPy = new Proyecto('GUSANITO-PYTHON', 'https://github.com/jsolano0112/Gusanito.git', 'img/gusanito.png',200,200, '')
 
 proyectosLista.push(pizarra, appControlGastos, gusanoPy)
 
@@ -31,7 +33,7 @@ function mostrarProjectos() {
 				<h6>${Proyecto.nombre}</h6>
 			</a>
             <p>${Proyecto.texto}</p>
-			<img src=${Proyecto.imagen} alt=${Proyecto.nombre}>
+			<img src=${Proyecto.imagen} width=${Proyecto.ancho} height=${Proyecto.alto} alt=${Proyecto.nombre}>
 		</article>
         `
         contenedorProjectos.innerHTML += opcionDeProyecto
